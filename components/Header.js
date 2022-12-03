47cd98d21ac59b1a64d2129c7880e3cffffd108b
LeamanB <LeamanBrown@gmail.com>
2022-12-03 14:35:11 -0600

    ad

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 
 export default state => html`
-  <header>
-    HHM
+  <h1>
+    <h1>${state.header}</h1>
   </header>
 `;
--- components/Nav.js
+++ components/Nav.js
@@ -5,5 +5,11 @@ export default links => html`
     <a href="./index.html">Home</a>
     <a href="./history.html">History</a>
     <a href="./music.html">Music</a>
+    ${links
+      .map(
+        link =>
+          `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
+      )
+      .join("")}
   </nav>
 `;
