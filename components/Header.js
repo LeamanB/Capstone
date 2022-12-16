5023f1e1802038946aae5a4629bff361df53f195
LeamanB <LeamanBrown@gmail.com>
2022-12-16 09:32:25 -0600

    views edit

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 
 export default state => html`
-  <h1>
-    <h1>${state.header}</h1>
+  <header>
+    HHM
   </header>
 `;
--- components/views/History.js
+++ components/views/History.js
@@ -2,27 +2,7 @@ import html from "html-literal";
 
 export default () => html`
   <section id="history">
-    <a>
-      <header>
-        HHM
-      </header>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-        />
-        <meta charset="UTF-8" />
-        <title>Holiday, History, & Music</title>
-        <link rel="stylesheet" href="style.css" />
-        <script type="module" src="./index.js"></script>
-      </head>
-      <body>
-        <nav>
-          <a href="./index.html">Home</a>
-          <a href="./history.html">History</a>
-          <a href="./music.html">Music</a>
-        </nav>
-        <hr />
+
         <h1>History</h1>
         <p>Learn the History</p>
         <div>
@@ -33,14 +13,5 @@ export default () => html`
         </div>
       </body>
 
-      <hr />
-      <footer>
-        <i>Listen, Learn, & Celebrate!</i>
-        <nav>
-          <a href="./about.html">About</a>
-          <a href="./contact.html">Contact</a>
-        </nav>
-      </footer>
-    </a>
   </section>
 `;
