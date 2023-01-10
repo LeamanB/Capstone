7b5e4f0f19a7e644b93751ded58a134d5d1cd4bf
LeamanB <115671278+LeamanB@users.noreply.github.com>
2023-01-10 17:00:33 -0600

    css text updates

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -4,25 +4,27 @@ export default () => html`
   <section id="contact">
     <h1>Contact</h1>
     <form
+      class="contactInfo"
       id="contactMessage"
       action="https://formspree.io/f/moqbzodo"
       method="POST"
     >
       <label>
         Your email:
-        <input type="email" name="email" />
+        <input id="contactTextarea" type="email" name="email" />
       </label>
+
       <label>
         Your message:
-        <textarea name="message"></textarea>
+        <textarea id="contactTextarea" name="message"></textarea>
       </label>
       <button type="submit">Send</button>
     </form>
 
     <p id="sendCor">Send all correspondence to:</p>
-    <a id="contactInfo" href="https://www.linkedin.com/in/leamanbrown/"
+    <a class="contactInfo" href="https://www.linkedin.com/in/leamanbrown/"
       >LinkedIN</a
     >
-    <a id="contactInfo" href="https://github.com/LeamanB">Github</a>
+    <a class="contactInfo" href="https://github.com/LeamanB">Github</a>
   </section>
 `;
